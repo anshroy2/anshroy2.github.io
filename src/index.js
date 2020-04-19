a8b401bd4313a8d9a8583ab3226b0bbab99bb4a5
anshroy2 <anshroytwo@gmail.com>
2020-04-18 23:10:41 -0500

    Fixed bootstrap error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(<App />, document.getElementById('app'));
 
