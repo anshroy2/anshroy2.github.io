70ce9587fcc88ed5bf6aa2005ddb23dcd310cfd2
anshroy2 <anshroytwo@gmail.com>
2020-04-19 01:47:29 -0500

    Added favicon and some other changes

--- src/components/JS/About.js
+++ src/components/JS/About.js
@@ -27,27 +27,48 @@ class About extends Component {
 	    		
 
 	    		<div class="Internships">
-		          <br/><hr/>
+		        	<br/><br/>
 
+		          	<div class="Company_with_Logo">
+		            	<a href="https://www.gep.com/" target="_blank" rel="noopener noreferrer">
+		              		<img alt="GEP" class="Logo" src="https://www4.gep.com/sites/all/themes/gep/images/logo.svg"/>
+		            	</a>
+		            	<div class="Description_bullet_points">
+		              		<h2 id="Title">Software Engineering Intern</h2>
+		              		<p class="Intern-Description">GEP is a provider of supply chain software, focusing on the procure to pay suite (in which <a href="https://www.gartner.com/doc/reprints?id=1-1OCNDGBV&ct=190802&st=sb?source_pageurl=https://www.gep.com/research-reports/thank-you?sourcenid=2866&type=research_report&source_pageurl=https%3A//www.gep.com/research-reports/gep-leader-in-gartner-magic-quadrant-procure-to-pay-suites">Gartner</a> ranked them in the top quadrant). I was in the Engineering division of GEP, focusing on the <a href="https://www.smartbygep.com/">SMART by GEP</a> platform. I worked on two projects in this internship.</p>
 
-		          <div id="Company_with_Logo">
-		            <a href="https://www.gep.com/" target="_blank" rel="noopener noreferrer">
-		              <img alt="GEP" class="Logo" src="https://www4.gep.com/sites/all/themes/gep/images/logo.svg"/>
-		            </a>
-		            <div class="Description_bullet_points">
-		              <h2 id="Title">Software Engineering Intern</h2>
-		              <p class="Intern-Description">GEP is a provider of supply chain software, focusing on the procure to pay suite (in which <a href="https://www.gartner.com/doc/reprints?id=1-1OCNDGBV&ct=190802&st=sb?source_pageurl=https://www.gep.com/research-reports/thank-you?sourcenid=2866&type=research_report&source_pageurl=https%3A//www.gep.com/research-reports/gep-leader-in-gartner-magic-quadrant-procure-to-pay-suites">Gartner</a> ranked them in the top quadrant). I was in the Engineering division of GEP, focusing on the <a href="https://www.smartbygep.com/">SMART by GEP</a> platform. I worked on two projects in this internship.</p>
+		              		<p class="Intern-Description">Created a backend service, utilizing Python-Flask as an API. Used string matching algorithms (such as Edit Distance) and synonym lists to match from one set of columns to another.</p>
 
-		              <p class="Intern-Description">Created a backend service, utilizing Python-Flask as an API. Used string matching algorithms (such as Edit Distance) and synonym lists to match from one set of columns to another.</p>
+		              		<p class="Intern-Description">Extracted JSON data from Azure DataLake using Apache Spark onto Azure docker. A script (in Python) ran on a docker that would de-serialize the JSON object and convert into a flat table.</p>
+		            	</div>
+					
+					</div>
 
-		              <text class="Intern-Description">Extracted JSON data from Azure DataLake using Apache Spark onto Azure docker. A script (in Python) ran on a docker that would de-serialize the JSON object and convert into a flat table.</text>
-		            </div>
-		          </div>
-		        </div>
+					{/* <br></br>
+
+					<div class="Company_with_Logo">
+		            	<a href="https://www.gep.com/" target="_blank" rel="noopener noreferrer">
+		              		<img alt="GEP" class="Logo" src="https://www4.gep.com/sites/all/themes/gep/images/logo.svg"/>
+		            	</a>
+		            	<div class="Description_bullet_points">
+		              		<h2 id="Title">Software Engineering Intern</h2>
+		              		<p class="Intern-Description">GEP is a provider of supply chain software, focusing on the procure to pay suite (in which <a href="https://www.gartner.com/doc/reprints?id=1-1OCNDGBV&ct=190802&st=sb?source_pageurl=https://www.gep.com/research-reports/thank-you?sourcenid=2866&type=research_report&source_pageurl=https%3A//www.gep.com/research-reports/gep-leader-in-gartner-magic-quadrant-procure-to-pay-suites">Gartner</a> ranked them in the top quadrant). I was in the Engineering division of GEP, focusing on the <a href="https://www.smartbygep.com/">SMART by GEP</a> platform. I worked on two projects in this internship.</p>
+
+		              		<p class="Intern-Description">Created a backend service, utilizing Python-Flask as an API. Used string matching algorithms (such as Edit Distance) and synonym lists to match from one set of columns to another.</p>
+
+		              		<p class="Intern-Description">Extracted JSON data from Azure DataLake using Apache Spark onto Azure docker. A script (in Python) ran on a docker that would de-serialize the JSON object and convert into a flat table.</p>
+		            	</div>
+					
+					</div> */}
+				
+
+					
+
+				</div>
+		    </div>
 
 	    		
 
-    		</div>
     	</div>
     );
   }
